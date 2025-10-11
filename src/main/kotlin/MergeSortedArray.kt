9abd04cd095d555288de9da08dfdf8cb2ed85cf6
Wahid <1213089@dtu.edu.eg>
2025-10-11 06:39:41 +0300

    Refactor merge function in MergeSortedArray class

--- src/main/kotlin/MergeSortedArray.kt
+++ src/main/kotlin/MergeSortedArray.kt
@@ -3,9 +3,9 @@ package org.example
 class MergeSortedArray {
 
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        var i = m - 1   // pointer in nums1
-        var j = n - 1   // pointer in nums2
-        var k = m + n - 1 // pointer at end of nums1
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
 
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
@@ -17,5 +17,4 @@ class MergeSortedArray {
             }
             k--
         }
-    }
-}
+    }}
